fef11ef10b70f7c863b829009d592492201803d7
ShivamMukherjee <mshivam98@gmail.com>
2017-07-10 20:41:07 +0530

    Renaming in the Contents

--- TestingGrounds/Source/TestingGrounds/FP_FirstPerson/FP_FirstPersonGameMode.cpp
+++ TestingGrounds/Source/TestingGrounds/FP_FirstPerson/FP_FirstPersonGameMode.cpp
@@ -9,7 +9,7 @@
 AFP_FirstPersonGameMode::AFP_FirstPersonGameMode()
 {
 	// Set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Character/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// Use our custom HUD class
