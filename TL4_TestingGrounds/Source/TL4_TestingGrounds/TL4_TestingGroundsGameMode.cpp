20c418f78ff880e9aa76a5b3c881eccaa77a007c
ShivamMukherjee <mshivam98@gmail.com>
2017-08-03 23:07:25 +0530

    Navigation pathways are now computed dynamically.

--- TL4_TestingGrounds/Source/TL4_TestingGrounds/TL4_TestingGroundsGameMode.cpp
+++ TL4_TestingGrounds/Source/TL4_TestingGrounds/TL4_TestingGroundsGameMode.cpp
@@ -11,7 +11,7 @@ ATL4_TestingGroundsGameMode::ATL4_TestingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behaviour/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/TL4_Character/Behaviour/TL4_Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
