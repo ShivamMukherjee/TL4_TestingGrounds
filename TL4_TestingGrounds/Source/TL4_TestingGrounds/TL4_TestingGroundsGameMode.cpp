575f0dcf111734d06db38714bfb860e651c352f6
ShivamMukherjee <mshivam98@gmail.com>
2017-08-11 11:45:01 +0530

    Refactoring to follow http://ue4.style

--- TL4_TestingGrounds/Source/TL4_TestingGrounds/TL4_TestingGroundsGameMode.cpp
+++ TL4_TestingGrounds/Source/TL4_TestingGrounds/TL4_TestingGroundsGameMode.cpp
@@ -11,7 +11,7 @@ ATL4_TestingGroundsGameMode::ATL4_TestingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/TL4_Character/Behaviour/TL4_Character"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/TL4_Character/Behaviour/BP_TL4Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
